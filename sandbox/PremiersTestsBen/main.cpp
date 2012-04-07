4d891aca7f998cf23349ccab389b9c62c864b75d
Benoit Djerigian <thetiuguy@gmail.com>
2012-04-07 15:58:37 +0200

    création de la sandbox Ben

---
+++ sandbox/PremiersTestsBen/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: thiut
+ *
+ * Created on 7 avril 2012, 15:56
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
